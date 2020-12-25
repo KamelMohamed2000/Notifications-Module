72e0fc12b23a5775e0ef71fe1e22365e4965cd85
Kamel_Mohamed <65584552+KamelMohamed2000@users.noreply.github.com>
2020-12-26 00:10:36 +0200

    Notifications API
    create - H2 database(springboot)

---
+++ ASS2 code/src/main/java/com/example/api/domain/NotificationTemplate.java
@@ -0,0 +1,73 @@
+package com.example.api.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class NotificationTemplate  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long templateId;
+    private String type;
+    private String Context;
+    private String Placeholder;
+    private String Languages;
+    private String Channels;
+
+    public NotificationTemplate(){
+
+
+    }
+
+    public NotificationTemplate(String type, String context, String placeholder,
+                                String languages, String channels) {
+        this.type = type;
+        Context = context;
+        Placeholder = placeholder;
+        Languages = languages;
+        Channels = channels;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getContext() {
+        return Context;
+    }
+
+    public void setContext(String context) {
+        Context = context;
+    }
+
+    public String getPlaceholder() {
+        return Placeholder;
+    }
+
+    public void setPlaceholder(String placeholder) {
+        Placeholder = placeholder;
+    }
+
+    public String getLanguages() {
+        return Languages;
+    }
+
+    public void setLanguages(String languages) {
+        Languages = languages;
+    }
+
+    public String getChannels() {
+        return Channels;
+    }
+
+    public void setChannels(String channels) {
+        Channels = channels;
+    }
+}
---
+++ ASS2 code/src/main/java/com/example/api/web/NotificationController.java
@@ -0,0 +1,57 @@
+package com.example.api.web;
+
+import com.example.api.dao.INotificationTemplateDataAccessLayer;
+import com.example.api.domain.NotificationTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class NotificationController {
+    @Autowired
+    private INotificationTemplateDataAccessLayer types;
+
+    @Autowired
+    private INotificationTemplateDataAccessLayer notiRepo;
+
+    //Home page
+    @GetMapping
+    public String welcome(){
+
+        return "welcome";
+    }
+    //end of home page
+
+    //create new temp
+    @GetMapping("/create")
+    public String Create(Model model){
+
+        model.addAttribute("template" ,new NotificationTemplate());
+        return "create";
+
+    }
+    @PostMapping("/create/success")
+    public String saveNewTemp(Model model , NotificationTemplate notiTemp){
+
+        List<NotificationTemplate> Types = types.findAll();
+
+        for (int i =0 ; i < Types.size() ; i++){
+            if (notiTemp.getType().equalsIgnoreCase(Types.get(i).getType())){
+                return "redirect:/create" ;
+            }
+        }
+
+        notiRepo.save(notiTemp);
+
+        return "redirect:/create";
+    }
+    //end of creating new temp
+
+}
